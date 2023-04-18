1c1fec50aef3f2baa29abbab87a3be5dea863702
Isadora Pilau <isadorapda@gmail.com>
2023-04-18 19:58:21 +0100

    fix: 🐛 fix svg module

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,7 +1,4 @@
 declare module '*.png'
 declare module '*.js'
 
-declare module '*.svg' {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
-  export default content
-}
+declare module '*.svg'
